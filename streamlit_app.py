79cf3bc9586134a63609b22245ff9b5130d60741
Manasasailaja <75979774+Manasasailaja@users.noreply.github.com>
2022-05-25 00:15:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruitvice_response=requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
 
   fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
-  return fruitvice_normalized
+  return fruityvice_normalized
 
 streamlit.header('FruityVice Fruit Advice!!')
 
